5aacffb1439c9dde8273a9f8c611d11fd8e5b01e
Chanyoung Park <rhdwkrxla68@gmail.com>
2025-09-04 14:39:38 +0900

    Refactor: Service Worker
    Offline worker를 IIEF로 분리

--- apps/client/public/sw.js
+++ apps/client/public/sw.js
@@ -1,20 +1,25 @@
-const OFFLINE_CACHE_NAME = 'offline-cache';
-const OFFLINE_URL = '/offline.html';
+/**
+ * Offline Worker
+ */
+(() => {
+  const OFFLINE_CACHE_NAME = 'offline-cache';
+  const OFFLINE_URL = '/offline.html';
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(OFFLINE_CACHE_NAME).then((cache) => {
-      return cache.add(OFFLINE_URL);
-    }),
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).catch(() => {
-        return caches.match(OFFLINE_URL);
+  self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open(OFFLINE_CACHE_NAME).then((cache) => {
+        return cache.add(OFFLINE_URL);
       }),
     );
-  }
-});
+  });
+
+  self.addEventListener('fetch', (event) => {
+    if (event.request.mode === 'navigate') {
+      event.respondWith(
+        fetch(event.request).catch(() => {
+          return caches.match(OFFLINE_URL);
+        }),
+      );
+    }
+  });
+})();
