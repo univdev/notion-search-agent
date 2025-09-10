e9aa50c86b6501909de0a263c1acb518ed57fc2a
Chanyoung Park <rhdwkrxla68@gmail.com>
2025-09-11 02:50:36 +0900

    Update: Check code quality
    CODE_RULES.md 파일 기준으로 코드 퀄리티 검토 후 이슈라이징 되는 부분 1차 수정

--- apps/client/src/apps/react-query/ReactQueryProvider.tsx
+++ apps/client/src/apps/react-query/ReactQueryProvider.tsx
@@ -2,10 +2,12 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ReactNode } from 'react';
 
+const DEFAULT_STALE_TIME = 1000 * 60 * 5;
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 1000 * 60 * 5,
+      staleTime: DEFAULT_STALE_TIME,
       refetchIntervalInBackground: false,
       retry: 2,
     },
